8c1b87266ac5ed2c18f3cd0831e0b4ec5541bc86
JPbonaldo <159311815+JPbonaldo@users.noreply.github.com>
2024-04-30 20:11:32 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  let randomSuit = pickSuit();
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".topSuit").classList.add(randomSuit);
+  document.querySelector(".bottomSuit").classList.add(randomSuit);
+
+  document.querySelector(".topSuit").innerHTML = randomSuit;
+  document.querySelector(".cardNumber").innerHTML = pickCardNumber();
+  document.querySelector(".bottomSuit").innerHTML = randomSuit;
+};
+
+let pickCardNumber = () => {
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomCardNumber = Math.floor(Math.random() * cardNumber.length);
+  return cardNumber[randomCardNumber];
+};
+
+let pickSuit = () => {
+  let suit = ["♥", "♣", "♦", "♠"];
+  let randomSuit = Math.floor(Math.random() * suit.length);
+  return suit[randomSuit];
 };
